18aff2c55683df072764d187eec56a64ffd2bdc5
のと <xsignsch001@gmail.com>
2025-01-19 14:48:54 +0900

    website done

--- src/components/layouts/FooterSection.tsx
+++ src/components/layouts/FooterSection.tsx
@@ -2,7 +2,7 @@ type FooterSectionProps = {
 	title: string;
 	children: React.ReactNode;
 };
-const FooterSection = ({ title, children }) => {
+const FooterSection = ({ title, children }: FooterSectionProps) => {
 	return (
 		<div className="md:w-1/4 w-full md:mb-0 mb-6">
 			<h4 className="text-md font-semibold mb-2">{title}</h4>
